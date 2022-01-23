4ff9e3cf0ddb2f4da172eeeeebf387742f556a9b
Lightningbamboo <50045806+Lightningbamboo@users.noreply.github.com>
2022-01-23 15:56:38 +0000

    Update tools_text.js

--- Script/tools_text.js
+++ Script/tools_text.js
@@ -1,7 +1,8 @@
-var tools_text=[
-"pi_calc",
-"body_height_calc"
-];
+var tools_text=[];
+	var i=0;
+window.addEventListener('message',function(e){tools_text.push(e.data.msg);});
+alert(tools_text.length);
+//document.getElementById("show_box").setAttribute("src","fu");
 var tools_intro=[
 "Nothing is imposiible",
 "Your Height By Your Parents!"
@@ -14,13 +15,21 @@ function find()
 	for(var a=0;a<tools_text.length;a++){
 		if(tools_text[a].indexOf(document.getElementById("search").value)!=-1){
 			document.getElementById("shower").innerHTML+=
-			"<strong>"+tools_text[a]+"</strong></br><a id='"+tools_text[a]+"' onclick='iframe_show();'>"+tools_intro[a]+"</a></br>"; 
+			"<h3>"+tools_text[a]+"</h3>\n<a id='"+a+"' onclick='iframe_show();'>"+tools_intro[a]+"</a>"; 
 			total_result++;
 		}
 		if(tools_text.length-a==1){}
 	}
 }
 function iframe_show(){
+	var data;
 	show_box_src=event.target.id;
 	document.getElementById("show_box").setAttribute("src","tools/"+show_box_src+".html");
+}
+setInterval("read()",100);
+function read(){
+	if(i<4){
+		document.getElementById("show_box").setAttribute("src","tools/"+i+".html");
+		i++;
+	}
 }
