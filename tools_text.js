3cd1aee43c89f4c0c220e41b3389aeb0c24968d9
Lightningbamboo <50045806+Lightningbamboo@users.noreply.github.com>
2022-01-24 08:29:21 +0800

    Update and rename Script/tools_text.js to tools_text.js

--- tools_text.js
+++ tools_text.js
@@ -1,6 +1,5 @@
 <div align="center">
-  <p><img src="RATTOR.png"/></p>
-  <p><h2>寻着你想要的工具...</h2></p>
+  <p><h1>寻着你想要的工具...</h1></p>
   <p><input style="width:350px" id="search"/></p>
   <p><button id="go" onclick="find();">在工具箱里搜索</button></p>
   <p><iframe id="show_box" src="caonima"></iframe></p>
